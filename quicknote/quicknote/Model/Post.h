340446a47e2b19882211e61b63c90b941f25fa38
Tom Silver <tomlikefuturam@gmail.com>
2014-03-15 02:19:02 +0800

    Post - Model

--- quicknote/quicknote/Model/Post.h
+++ quicknote/quicknote/Model/Post.h
@@ -20,12 +20,27 @@
 @property (atomic, retain) NSMutableArray *fieldArray;
 @property (atomic, retain) NSMutableDictionary *fieldValueDict;
 
+@property (atomic, retain) NSString *tableTag;
+@property (atomic, retain) NSString *tableMark;
+@property (atomic, retain) NSString *tablePostToTag;
+@property (atomic, retain) NSString *tablePostToMark;
+
 @property (atomic, assign) NSInteger postId;
 @property (atomic, retain) NSString *name;
+@property (atomic, retain) NSString *nameIndex;
 @property (atomic, retain) NSString *description;
+@property (atomic, retain) NSString *location;
+@property (atomic, retain) NSString *address;
+@property (atomic, retain) NSString *longitude;
+@property (atomic, retain) NSString *latitude;
+@property (atomic, retain) NSString *watermark;
+@property (atomic, assign) NSInteger created;
+@property (atomic, assign) NSInteger updated;
 
+- (BOOL)fetch:(NSMutableArray *)postArray Filter:(NSString *)filter Order:(NSString *)order Page:(NSInteger)page PageSize:(NSInteger)pageSize;
 
 - (BOOL)add;
+- (BOOL)update;
 - (BOOL)remove;
 
 @end
--- quicknote/quicknote/Model/Post.m
+++ quicknote/quicknote/Model/Post.m
@@ -17,7 +17,12 @@
     
     if (self) {
         self.table = @"post";
-        self.fieldArray = [[NSMutableArray alloc] initWithArray:@[@"post_id", @"name", @"description"]];
+        self.tablePostToTag = @"post_to_tag";
+        self.tablePostToMark = @"post_to_mark";
+        self.tableTag = @"tag";
+        self.tableMark = @"mark";
+        
+        //self.fieldArray = [[NSMutableArray alloc] initWithArray:@[@"post_id", @"name", @"description"]];
         //self.fieldValueDict = [[NSMutableDictionary alloc] init];
     }
     
@@ -25,6 +30,22 @@
     
 }
 
+- (BOOL)fetch:(NSMutableArray *)postArray Filter:(NSString *)filter Order:(NSString *)order Page:(NSInteger)page PageSize:(NSInteger)pageSize
+{
+    if (!self.db) {
+        return NO;
+    }
+    
+    NSString *sql = @"SELECT * FROM %@ AS p INNER JOIN %@ AS pt ON p.post_id=pt.post_id INNER JOIN %@ AS t ON pt.tag_id=t.tag_id WHERE %@ ORDER BY %@ LIMIT %d, %d";
+    
+    FMResultSet *result = [self.db executeQueryWithFormat:sql, self.table, self.tablePostToTag, self.tableTag, filter, order, page * pageSize, pageSize];
+    
+    while ([result next]) {
+        
+    }
+    
+    return YES;
+}
 
 - (BOOL)add
 {
@@ -32,13 +53,63 @@
         return NO;
     }
     
-    [self.db executeUpdateWithFormat:@"INSERT INTO %@ (post_id, name, description) VALUES (%d, %@, %@)", self.table, self.postId, self.name, self.description];
+    NSString *sql = @"INSERT INTO %@ (name, name_index, description, location, address, longitude, latitude, watermark, created, updated) VALUES (%@, %@, %@, %@, %@, %@, %@, %@, %ld, %ld)";
+    
+    [self.db executeUpdateWithFormat:sql, self.name, self.nameIndex, self.description, self.location, self.address, self.longitude, self.latitude, self.watermark, self.created, self.updated];
+    
+    self.postId = (NSInteger)[self.db lastInsertRowId];
+    
+    return YES;
+}
+
+- (BOOL)update
+{
+    if (!self.db) {
+        return NO;
+    }
+    
+    NSString *sql = @"UPDATE %@ SET name=%@, name_index=%@, description=%@, location=%@, address=%@, longitude=%@, latitude=%@, watermark=%@, created=%ld, updated=%ld WHERE post_id=%ld";
+    
+    [self.db executeUpdateWithFormat:sql, self.name, self.nameIndex, self.description, self.location, self.address, self.longitude, self.latitude, self.watermark, self.created, self.updated, self.postId];
+    
+    return YES;
+}
+
+- (BOOL)updateTag:(NSArray *)tagArray
+{
+    if (!self.db) {
+        return NO;
+    }
+    
+    NSString *sql = @"UPDATE %@ SET post_id=%ld, tag_id=%ld, updated=%ld WHERE post_id=%ld";
+    
+    //[self.db executeUpdateWithFormat:sql, self.postId, tagId, self.updated];
+    
+    return YES;
+}
+
+- (BOOL)updateMark
+{
+    if (!self.db) {
+        return NO;
+    }
+    
+    NSString *sql = @"UPDATE %@ SET post_id=%ld, mark_id=%ld, updated=%ld WHERE post_id=%ld";
+    
+    //[self.db executeUpdateWithFormat:sql, self.postId, tagId, self.updated];
     
     return YES;
 }
 
 - (BOOL)remove
 {
+    if (!self.db) {
+        return NO;
+    }
+    
+    NSString *sql = @"DELETE FROM %@ WHERE post_id=%d";
+    
+    [self.db executeUpdateWithFormat:sql, self.table, self.postId];
     
     return YES;
 }
