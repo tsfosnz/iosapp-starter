dc43be065dbd72a8cac1372c886c39d36d16e28e
Tom Silver <tomlikefuturam@gmail.com>
2014-04-09 02:26:48 +0800

    update

---
+++ quicknote/quicknote/add/AddController.h
@@ -0,0 +1,13 @@
+//
+//  AddController.h
+//  quicknote
+//
+//  Created by hello on 14-4-9.
+//  Copyright (c) 2014年 hellomaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddController : UIViewController
+
+@end
